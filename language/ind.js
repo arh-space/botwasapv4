d9a417f262bbeca8c63303f4d4934cd4e47fdd4d
ARH <65483182+arh-space@users.noreply.github.com>
2021-01-30 09:33:16 +0700

    Update ind.js

--- language/ind.js
+++ language/ind.js
@@ -1,41 +1,41 @@
 exports.wait = () => {
-	return`*「 WAIT 」 SEDANG PROSES*`
+	return`*「 WAIT 」 Tunggu sebentar yaa sedang di proses~*`
 }
 
 exports.succes = () => {
-	return`*「 SUCCES 」*`
+	return`*「 SUCCES 」Okeee udah selesai~*`
 }
 
 exports.lvlon = () => {
-	return`*「 ENABLE 」 LEVELING*`
+	return`*「 ENABLE 」 Fitur Leveling di Aktifkan*`
 }
 
 exports.lvloff = () => {
-	return`*「 DISABLE 」 LEVELING*`
+	return`*「 DISABLE 」 Fitur Leveling di Non-Aktifkan*`
 }
 
 exports.lvlnul = () => {
-	return`*LEVELMU MASIH KOSONG*`
+	return`*「 INFO 」 Hai kak! Level Kamu masih 0, Silahkan Leveling terlebih dahulu yaa~*`
 }
 
 exports.lvlnoon = () => {
-	return`*LEVEL DI GRUB BELUM DI AKTIFKAN*`
+	return`*「 INFO 」Hai! Fitur Level di Group belum di Aktifkan*`
 }
 
 exports.noregis = () => {
-	return`*「 BELUM DAFTAR 」*\n\n*cara daftar ${prefix}daftar nama|umur* \n*contoh ${prefix}daftar affis|17*`
+	return`*「 BELUM DAFTAR 」*\n\n*Hai kak! Maaf Kamu belum terdaftar sebagai User MYTH Bot :(\n\nSilahkan daftar terlebih dahulu ya :)\n\nCara daftar :  ${prefix}daftar Nama | Umur* \n*Contoh : ${prefix}daftar Rawrrr | 17*`
 }
 
 exports.rediregis = () => {
-	return`*「 SUDAH DAFTAR 」*\n\n*kamu sudah terdaftar di database bot*`
+	return`*「 SUDAH DAFTAR 」*\n\n*Hai kak! kamu sudah terdaftar sebagai user di Database Myth bot*`
 }
 
 exports.stikga = () => {
-	return`*yah gagal coba ulangi beberapa saat lagi*`
+	return`*「 ERROR 」Aduh Gagal! :< Coba ulangi beberapa saat lagi yaa~*`
 }
 
 exports.linkga = () => {
-	return`*maaf link tidak valid*`
+	return`*「 INFO 」Aduh maaf link tidak valid!*`
 }
 
 exports.groupo = () => {
@@ -59,27 +59,27 @@ exports.badmin = () => {
 }
 
 exports.nsfwoff = () => {
-	return`*NSFW GAK AKTIF*`
+	return`*「 INFO 」Fitur NSFW Belum di Aktifkan*`
 }
 
 exports.bug = () => {
-	return`*Masalah telah di laporkan ke owner BOT, laporan palsu/main2 tidak akan ditanggapi*`
+	return`*「 REPORT 」Masalah telah di laporkan ke owner BOT, laporan palsu/main2 tidak akan ditanggapi*`
 }
 
 exports.wrongf = () => {
-	return`*format salah/text kosong*`
+	return`*「 ERROR 」Hai kak! Maaf Format salah/text kosong*`
 }
 
 exports.clears = () => {
-	return`*clear all Success*`
+	return`*「 INFO 」 Clear all Success*`
 }
 
 exports.pc = () => {
-	return`*「 REGISTRASI 」*\n\nuntuk mengetahui apa kamu sudah terdaftar silahkah check message yang saya kirim \n\nNOTE:\n*jika kamu belum mendapatkan pesan. berarti kamu belum menyimpan nomer bot*`
+	return`*「 REGISTRASI 」*\n\nuntuk mengetahui apa kamu sudah terdaftar silahkah check message yang saya kirim \n\nNOTE:\n*jika kamu belum mendapatkan pesan. berarti kamu belum menyimpan nomor bot*`
 }
 
 exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
-	return`*「 DATA NEGARA 」*\n\nkamu sudah terdaftar dengan data \n\n┏━⊱nama\n┗⊱${namaUser}\n┏━⊱nomer\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱umur\n┗⊱${umurUser}\n┏━⊱waktu pendaftaran\n┗⊱${time}\n\n┏━❉ *NS* ❉━\n┣⊱${serialUser}\n┗⊱NOTE : jangan sampai lupa nomer ini karena ini penting:v`
+	return`*「 DATA USER 」*\n\nHai kak! Selamat kamu sudah berhasil mendaftar sebagai user MYTH Bot dengan data berikut: \n\n┏━⊱Nama\n┗⊱${namaUser}\n┏━⊱Nomor\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱Umur\n┗⊱${umurUser}\n┏━⊱Waktu Pendaftaran\n┗⊱${time}\n\n┏━❉ *Nomor Serial* ❉━\n┣⊱${serialUser}\n┗⊱NOTE : Save Nomor Serial ini`
 }
 
 exports.cmdnf = (prefix, command) => {
@@ -87,43 +87,43 @@ exports.cmdnf = (prefix, command) => {
 }
 
 exports.owneresce = (pushname) => {
-	return`*maaf tapi ${pushname} bukan owner script*`
+	return`*「 INFO 」Maaf tapi ${pushname} bukan Owner*`
 }
 
 exports.reglevelaha = (command, pushname, getLevelingLevel, sender, aha) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${aha}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*「 INFO 」Maaf ${pushname} level Kamu belum mencukupi*\n\n*┏⊱level : ${getLevelingLevel(sender)}*\n*┣⊱Jenis Command : ${command}*\n*┗⊱Syarat Level : ${aha}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
 }
 
 exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*「 INFO 」Maaf ${pushname} level Kamu belum mencukupi*\n\n*┏⊱Level : ${getLevelingLevel(sender)}*\n*┣⊱Jenis Command : ${command}*\n*┗⊱Syarat Level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
 }
 
 exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahc}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*「 INFO 」Maaf ${pushname} level Kamu belum mencukupi*\n\n*┏⊱Level : ${getLevelingLevel(sender)}*\n*┣⊱Jenis Command : ${command}*\n*┗⊱Syarat Level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
 }
 
 exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahd}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*「 INFO 」Maaf ${pushname} level Kamu belum mencukupi*\n\n*┏⊱Level : ${getLevelingLevel(sender)}*\n*┣⊱Jenis Command : ${command}*\n*┗⊱Syarat Level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
 }
 
 exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahe}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*「 INFO 」Maaf ${pushname} level Kamu belum mencukupi*\n\n*┏⊱Level : ${getLevelingLevel(sender)}*\n*┣⊱Jenis Command : ${command}*\n*┗⊱Syarat Level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
 }
 
 exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahf}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*「 INFO 」Maaf ${pushname} level Kamu belum mencukupi*\n\n*┏⊱Level : ${getLevelingLevel(sender)}*\n*┣⊱Jenis Command : ${command}*\n*┗⊱Syarat Level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
 }
 
 exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangku) => { 
 	return `
 ┏━━━⊱  *ABOUT USER*  ⊰━━┓
 ┣⊱ *Nama* : ${pushname}
-┣⊱ *Nomer* : wa.me/${sender.split("@")[0]}
-┣⊱ *Uang mu* : Rp${uangku}
+┣⊱ *Nomor* : wa.me/${sender.split("@")[0]}
+┣⊱ *Wallet* : Rp${uangku}
 ┣⊱ *XP* : ${getLevelingXp(sender)}/${reqXp}
 ┣⊱ *Level* : ${getLevelingLevel(sender)}
 ┣⊱ *User register* : ${_registered.length}
-┗━━━⊱  ⸨ *AFFIS* ⸩  ⊰━━━━┛
+┗━━━⊱  ⸨ *MYTH* ⸩  ⊰━━━━┛
 
 
 ┏━━⊱ *MAKER MENU* ⊰━━┓
@@ -140,6 +140,7 @@ exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp
 ┣⊱ *${prefix}firework*
 ┣⊱ *${prefix}lava*
 ┣⊱ *${prefix}qrcode*
+
 ┣━━⊱  *FUN MENU*  ⊰━━━┫
 ┣⊱ *${prefix}mining*
 ┣⊱ *${prefix}bisakah*
@@ -153,9 +154,11 @@ exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp
 ┣⊱ *${prefix}nangis*
 ┣⊱ *${prefix}cium*
 ┣⊱ *${prefix}peluk*
+
 ┣━━⊱ *MUTUAL* ⊰━━━━━┫
 ┣⊱ *${prefix}mutual*
 ┣⊱ *${prefix}next*
+
 ┣━━⊱ *MEDIA MENU* ⊰━━┫
 ┣⊱ *${prefix}quotes*
 ┣⊱ *${prefix}beritahoax*
@@ -164,10 +167,12 @@ exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp
 ┣⊱ *${prefix}resepmasakan*
 ┣⊱ *${prefix}igstalk*
 ┣⊱ *${prefix}bitly*
+
 ┣━━⊱ *YT & SONG* ⊰━━━┫
 ┣⊱ *${prefix}ytmp3*
 ┣⊱ *${prefix}ytmp4*
 ┣⊱ *${prefix}joox*
+
 ┣━━⊱  *NSFW MENU*  ⊰━┫
 ┣⊱ *${prefix}pokemon*
 ┣⊱ *${prefix}anjing*
@@ -175,11 +180,13 @@ exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp
 ┣⊱ *${prefix}nekonime*
 ┣⊱ *${prefix}husbu*
 ┣⊱ *${prefix}ranime*
+
 ┣━━⊱ *LIMIT & UANG* ⊰━┫
 ┣⊱ *${prefix}limit*
 ┣⊱ *${prefix}buylimit*
 ┣⊱ *${prefix}transfer*
 ┣⊱ *${prefix}dompet*
+
 ┣━━⊱ *GROUP MENU* ⊰━┫
 ┣⊱ *${prefix}hidetag*
 ┣⊱ *${prefix}grouplist*
@@ -199,6 +206,7 @@ exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp
 ┣⊱ *${prefix}nsfw* [1/0]
 ┣⊱ *${prefix}simih* [1/0]
 ┣⊱ *${prefix}welcome* [1/0]
+
 ┣━━⊱ *OWNER MENU* ⊰━┫
 ┣⊱ *${prefix}bc*
 ┣⊱ *${prefix}bcgc*
@@ -212,29 +220,16 @@ exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp
 ┣⊱ *${prefix}event* [1/0]
 ┣⊱ *${prefix}clone*
 ┣⊱ *${prefix}setppbot*
-┣━━⊱ *THANGKS TO* ⊰━┫
-┃
-┣⊱ *AFFIS JUNIANTO*
-┣⊱ *FADHIL GRAPHY*
-┣⊱ *MHANKBARBARS*
-┣⊱ *MYBOT TEAM*
-┣⊱ *AGUNG(XPTN)*
-┣⊱ *BRYAN(SUHU)*
-┣⊱ *TOBZ*
-┣⊱ *ARUGAZ*
-┃
-┣⊱ NOTE : TQTO DI HAPUS
-┃ GW GAK UP LAGI OKE
-┃
-┗━━⊱  ⸨ *AFFIS* ⸩  ⊰━━━┛
+
+┗━━⊱  ⸨ *MYTH* ⸩  ⊰━━━┛
 `
 }
 
 exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel) => {
 	return`
-*「 SELAMAT 」*
+*「 Congratulation 」*
 ┏⊱ *Nama* : ${pushname}
-┣⊱ *Nomer* : wa.me/${sender.split("@")[0]}
+┣⊱ *Nomor* : wa.me/${sender.split("@")[0]}
 ┣⊱ *Xp* : ${getLevelingXp(sender)}
 ┣⊱ *Limit* = +3
 ┗⊱ *Level* : ${getLevel} ⊱ ${getLevelingLevel(sender)}
@@ -257,5 +252,5 @@ exports.satukos = () => {
 }
 
 exports.uangkau = (pushname, sender, uangkau) => {
-	return`*┏⊱ 「 ATM 」⊰━┓*\n┣⊱ *Nama* : ${pushname}\n┣⊱ *Nomer* : ${sender.split("@")[0]}\n┣⊱ *Uang* : ${uangkau}\n┗━━━━━━━━━━`
+	return`*┏⊱ 「 ATM 」⊰━┓*\n┣⊱ *Nama* : ${pushname}\n┣⊱ *Nomor* : ${sender.split("@")[0]}\n┣⊱ *Uang* : ${uangkau}\n┗━━━━━━━━━━`
 }
